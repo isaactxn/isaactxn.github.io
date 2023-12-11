67011b2de0347ee6046cfa572ddb1c58995db372
isaac <isaactxn+dev@gmail.com>
2023-12-10 20:14:11 -0600

    ISAAC'S INTERLUDES
    YAYAY

---
+++ js/music-list.js
@@ -0,0 +1,62 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "We Are the Champions",
+    artist: "Queen",
+    performer: "Performed by Isaac Nguyen",
+    img: "cover",
+    src: "wearethechampions"
+  },
+  {
+    name: "Por una Cabeza",
+    artist: "Carlos Gardel",
+    performer: "Performed by Isaac Nguyen",
+    img: "cover",
+    src: "porunacabeza"
+  },
+  {
+    name: "Take Five",
+    artist: "Paul Desmond",
+    performer: "Performed by Isaac Nguyen",
+    img: "cover",
+    src: "takefive"
+  },
+  {
+    name: "Where is my mind",
+    artist: "Pixies",
+    performer: "Performed by Isaac Nguyen",
+    img: "cover",
+    src: "whereismymind"
+  },
+  {
+    name: "Moonlight Sonata\n(1st movement)",
+    artist: "Ludwig van Beethoven",
+    performer: "Performed by Isaac Nguyen",
+    img: "cover",
+    src: "moonlightsonata"
+  },
+  {
+    name: "Chiquitita",
+    artist: "ABBA",
+    performer: "Performed by Isaac Nguyen",
+    img: "cover",
+    src: "chiquitita"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
